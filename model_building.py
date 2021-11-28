04a168b3fef352e9ee6693a98a9a6b17db87f573
NguyenThai1705 <nguyenlqda2@gmail.com>
2021-11-28 10:36:00 +0700

    created flask api endpoint

--- model_building.py
+++ model_building.py
@@ -16,7 +16,7 @@ df.columns
 df.shape
 df_model = df[['Rating', 'Size', 'Type of ownership', 'Industry', 'Sector', 'Revenue', 'comp_count', 'per_hour',
             'employer_provided', 'age', 'python', 'r_studio', 'spark', 'aws', 'excel', 'title_simp', 'seniority',
-            'descrip_len', 'avg_salary']]
+            'avg_salary']]
 
 #get dummy data
 df_dum = pd.get_dummies(df_model)
@@ -72,7 +72,6 @@ np.mean(cross_val_score(rf, X_train, y_train, scoring='neg_mean_absolute_error',
 
 #tune models GridsearchCV
 from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
 
 rf_parameters = {'n_estimators': range(10,300,10), 
                  'criterion':('mse', 'mae'), 
@@ -81,7 +80,7 @@ rf_parameters = {'n_estimators': range(10,300,10),
 gs = GridSearchCV(rf, param_grid=rf_parameters, scoring='neg_mean_absolute_error', cv=3)
 gs.fit(X_train, y_train)
 
-
+gs.best_estimator_
 
 #test ensembles
 tpred_lm = lm.predict(X_test)
@@ -92,3 +91,28 @@ from sklearn.metrics import mean_absolute_error
 print(mean_absolute_error(y_test, tpred_lm))
 print(mean_absolute_error(y_test, tpred_lml))
 print(mean_absolute_error(y_test, tpred_gs))
+
+#saved model
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ))
+
+#test model
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+model.predict(X_test.iloc[1,:].values.reshape(1, -1))
+X_test.iloc[1,:].values
+
+#how to convert new data to data dummies
+columns_model = df_model.columns.drop('avg_salary')
+newdata = [[3.8,'501 to 1000 employees','Company - Private','Aerospace & Defense',
+            'Aerospace & Defense','$50 to $100 million (USD)',0,0,0,48,1,0,0,0,1,'data scientist','na']]
+
+newdf = pd.DataFrame(newdata, columns=columns_model)
+newdf = newdf.reindex(labels = df_dum.columns, axis = 1, fill_value = 0).drop(columns='avg_salary')
+
+model.predict(newdf.values)
+
